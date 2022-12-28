9bdd3825ae97c4cdf2050585735be6dc90fa9d3c
Kosolapik <sana.kosolapov@ya.ru>
2022-12-28 21:19:54 +0500

    изменения

--- checkCurl.php
+++ checkCurl.php
@@ -3,19 +3,29 @@
     require_once  'curlWrapper.php';
 
     $params = [
-        'name' => 'Aleksandr',
-        'email' => 'sana.kosolapov@ya.ru'
+        'query' => 'чус',
+        'contentType' => 'city',
+        'withParent' => 1,
+        'limit' => 10,
+        // 'regionId' => 5900000000000
     ];
     
     $headers = [
+       
     ];
     $req = new CurlWrapper($headers);
-    $requer = $req->sendRequest('get', 'https://httpbin.org/anything', $params);
+    $requer = $req->sendRequest('get', 'https://kladr-api.ru/api.php', $params);
+    $requer = json_decode($requer, true);
 
 ?>
 
 <pre>
     <?php
-        print_r($requer);
+        print_r ($requer);
     ?>
 </pre>
+<!-- <pre>
+    <?php
+        echo ($requer);
+    ?>
+</pre> -->
--- curlWrapper.php
+++ curlWrapper.php
@@ -35,62 +35,14 @@ class CurlWrapper {
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $result = curl_exec($ch);
         curl_close($ch);
-        if(gettype($result) == 'string') {
-            $result = json_decode($result, true);
-        }
+        $result = json_decode($result, true);
+        $result = json_encode($result, JSON_UNESCAPED_UNICODE);
+        // $result = gettype($result);
+        // if(gettype($result) == 'string') {
+        //     $result = json_decode($result, true);
+        // }
         return $result;
     }
-
-    // public function post(string $url = '', array $params = []) {
-    //     if ($url === '' || $url === null || $url === false) {
-    //         echo 'При вызове метода post задайте параметр типа строки с URL адресом';
-    //     } else {
-    //         $ch = curl_init($url);
-    //     }
-
-    //     if ($this->headers !== '' && $this->headers !== null && $this->headers !== false) {
-    //         curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
-    //     }
-
-    //     curl_setopt($ch, CURLOPT_POST, true);
-    //     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-
-    //     curl_setopt($ch, CURLOPT_HEADER, false);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    //     $result = curl_exec($ch);
-    //     curl_close($ch);
-    //     if(gettype($result) == 'string') {
-    //         $result = json_decode($result, true);
-    //     }
-    //     return $result;
-    // }
-
-    // public function json(string $url = '', array $params = []) {
-    //     if ($url === '' || $url === null || $url === false) {
-    //         echo 'При вызове метода json задайте параметр типа строки с URL адресом';
-    //     } else {
-    //         $ch = curl_init($url);
-    //     }
-
-    //     if ($this->headers !== '' && $this->headers !== null && $this->headers !== false) {
-    //         curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
-    //     }
-
-    //     $this->header[] = 'Content-Type:application/json';
-    //     curl_setopt($ch, CURLOPT_POST, true);
-    //     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params, JSON_UNESCAPED_UNICODE));
-
-    //     curl_setopt($ch, CURLOPT_HEADER, false);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    //     $result = curl_exec($ch);
-    //     if(gettype($result) == 'string') {
-    //         $result = json_decode($result, true);
-    //     }
-    //     curl_close($ch);
-    //     return $result;
-    // }
 
 }
 
